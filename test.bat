430e3a7e0f359de2f3f44663c42f7b35750773dc
RYUK <suhailalhegry@gmail.com>
2020-12-10 20:19:57 +0300

    added support for clang on windows using clang-cl

--- test.bat
+++ test.bat
@@ -13,29 +13,59 @@ set CommonLinks=/link /incremental:NO /libpath:../lib shlwapi.lib
 set BuildDir=win32
 set Release=false
 set Debug=false
+set UseClang=false
 
 REM BUILDING
+REM find a better way to parse arguments
 IF [%~1] == [-r] (
     set BuildDir=win32_release
     set Release=true
 
     IF [%~2] == [-d] (
         set Debug=true
+
+        IF [%~3] == [-c] (
+            set UseClang=true
+        )
+    )
+
+    IF [%~2] == [-c] (
+        set UseClang=true
     )
 )
 
 IF [%~1] == [-d] (
     set Debug=true
+
+    IF [%~2] == [-c] (
+        set UseClang=true
+    )
 )
 
-IF [%Release%] == [true] (
-    @echo on
-    cl %CommonFlags% %CommonIncludes% /Fe%BuildDir%/ /Fo%BuildDir%/ /O2 /Ot %CommonSourceFiles% %CommonLinks%
-    @echo off
+IF [%~1] == [-c] (
+    set UseClang=true
+)
+
+IF [%UseClang%] == [true] (
+    IF [%Release%] == [true] (
+        @echo on
+        clang-cl %CommonFlags% %CommonIncludes% /Fe%BuildDir%/ /Fo%BuildDir%/ /O2 /Ot %CommonSourceFiles% %CommonLinks%
+        @echo off
+    ) ELSE (
+        @echo on
+        clang-cl %CommonFlags% %CommonIncludes% /Zi /Fe%BuildDir%/ /Fo%BuildDir%/ /Fd%BuildDir%/ /Fa%BuildDir%/ %CommonSourceFiles% %CommonLinks%
+        @echo off
+    )
 ) ELSE (
-    @echo on
-    cl %CommonFlags% %CommonIncludes% /DRETEX_DEBUG /DEBUG:FULL /Zi /Fe%BuildDir%/ /Fo%BuildDir%/ /Fd%BuildDir%/ /Fa%BuildDir%/ /Fm%BuildDir%/ /Fp%BuildDir%/ /FR%BuildDir%/ %CommonSourceFiles% %CommonLinks%
-    @echo off
+    IF [%Release%] == [true] (
+        @echo on
+        cl %CommonFlags% %CommonIncludes% /Fe%BuildDir%/ /Fo%BuildDir%/ /O2 /Ot %CommonSourceFiles% %CommonLinks%
+        @echo off
+    ) ELSE (
+        @echo on
+        cl %CommonFlags% %CommonIncludes% /DEBUG:FULL /Zi /Fe%BuildDir%/ /Fo%BuildDir%/ /Fd%BuildDir%/ /Fa%BuildDir%/ /Fm%BuildDir%/ /Fp%BuildDir%/ /FR%BuildDir%/ %CommonSourceFiles% %CommonLinks%
+        @echo off
+    )
 )
 @echo off
 pushd %BuildDir%
